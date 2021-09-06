30f5fe8b18a5a21234319c99a53e7d3d7be52cbc
Keve Lajsz <kevelajsz@icloud.com>
2021-09-06 14:17:30 +0200

    Added Google Drive folder watcher

---
+++ src/watcher.ts
@@ -0,0 +1,36 @@
+import { google } from 'googleapis';
+import { OAuth2Client } from 'google-auth-library';
+
+const DRIVE_PRINT_DIR_ID = '1yX7U0cUoT-UntZwtP8hkRJ5QZFkPdKWs';
+const CONTENT_CHECK_INTERVAL = 8000; //ms
+
+export function onPrintDirContentChange(
+  auth: OAuth2Client, callback: (files: string[]) => void
+) {
+  const drive = google.drive({ version: 'v3', auth: auth });
+
+  let oldContent: string[] = [];
+  let i = 0;
+
+  setInterval(() => {
+    drive.files.list({
+      q: `'${ DRIVE_PRINT_DIR_ID }' in parents and trashed=false`,
+      spaces: 'drive',
+      pageSize: 10,
+      fields: 'nextPageToken, files(id)'
+    }, (err, response) => {
+      if (err || !response) throw new Error(`API (getting files): ${ err }`);
+
+      const newContent = response.data.files!.map(({ id }) => id!);
+      const difference = newContent.filter((file) => !oldContent.includes(file));
+      oldContent = [ ...newContent ];
+
+      if (i !== 0 && !!difference) {
+        console.log('New files uploaded:', difference);
+        callback(difference);
+      }
+
+      i++;
+    });
+  }, CONTENT_CHECK_INTERVAL);
+}
